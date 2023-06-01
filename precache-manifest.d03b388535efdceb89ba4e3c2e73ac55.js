2a509280dd07156f5908eb59ee4ccef49398fa20
+ <dfarley1@binghamton.edu>
2023-06-01 16:51:31 -0400

    Updates

---
+++ precache-manifest.d03b388535efdceb89ba4e3c2e73ac55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e83a342cc557724a9133",
+    "url": "/dennis_farley_portfolio/static/css/main.82f3d8fd.chunk.css"
+  },
+  {
+    "revision": "e83a342cc557724a9133",
+    "url": "/dennis_farley_portfolio/static/js/main.e83a342c.chunk.js"
+  },
+  {
+    "revision": "06cea806515d57df58fc",
+    "url": "/dennis_farley_portfolio/static/js/1.06cea806.chunk.js"
+  },
+  {
+    "revision": "f027c931c4f7914340fb",
+    "url": "/dennis_farley_portfolio/static/js/runtime~main.f027c931.js"
+  },
+  {
+    "revision": "699612587a2c809bbd3cc505263e706b",
+    "url": "/dennis_farley_portfolio/index.html"
+  }
+];
