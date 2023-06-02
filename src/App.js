6e972a119b2c2d23f9d93237d375232ceeb34422
+ <dfarley1@binghamton.edu>
2023-06-02 13:18:55 -0400

    add contact link +route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import './App.css';
 import Home from "./Home";
+import Contact from "./Contact";
 import { Link, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
       <nav className="navbar">
         <Link classname = "link" to='/' exact="true">Home</Link>
+        <Link classname = "link" to='/contact' exact="true"> Contact</Link>
         <br/>
       </nav>
       <Routes>
         <Route path='/' exact="true" element={<Home />} />
+        <Route path='/contact' exact="true" element={<Contact />} />
      </Routes>
     </div>
   );
---
+++ src/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Contact() {
+
+  return (
+    <>
+    <body classname="contact">
+        <header>
+
+      <h1 className='Contact'>
+        Contact Information 
+      </h1>
+        <p>Cell: 718-730-2373</p>
+        <p>Email: dfarley1@binghamton.edu</p>
+        <p>Github: https://github.com/dfarlz97</p>
+        <p>Github: https://github.com/dfarlz97</p>
+        <p>Blog: https://dev.to/dfarlz97</p>
+
+        </header>
+     
+      </body>
+    </>
+  )
+}
